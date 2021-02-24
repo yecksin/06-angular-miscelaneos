cbc4267f55c51adc47c5350f8d54b7a2e3a7ab26
Yecksin Mauricio <yecksin@gmail.com>
2021-02-23 23:38:08 -0500

    ciclo de vida de componentes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log("constructor");
+   }
 
   ngOnInit(): void {
+    console.log("ngOnInit");
+  }
+
+  ngOnChanges(): void {
+    console.log("ngOnChanges");
+
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck");
+    console.log("detecta un ccambio");
+  }
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit");
+    console.log("antes de hacer el ccambio");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+    console.log("despues de hacer el ccambio");
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy");
+
   }
 
 }
